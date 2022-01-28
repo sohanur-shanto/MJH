aac4da93d1b9873b07d688e6616aaadcd7fb30be
sohanur-shanto <57097733+sohanur-shanto@users.noreply.github.com>
2022-01-28 17:21:05 +0600

    Update updater.py

--- jobs/updater.py
+++ jobs/updater.py
@@ -1,9 +1,9 @@
 # from apscheduler.schedulers.background import BackgroundScheduler
 # from .jobs import my_scheduled_job, my_email, my_payment
 
-def start():
-    scheduler = BackgroundScheduler()
-    # scheduler.add_job(my_scheduled_job, 'interval', hours=2)
-    # scheduler.add_job(my_email, 'interval', minutes=1)
-    # scheduler.add_job(my_payment, 'interval', minutes=1)
-    scheduler.start()
+# def start():
+#     scheduler = BackgroundScheduler()
+#     # scheduler.add_job(my_scheduled_job, 'interval', hours=2)
+#     # scheduler.add_job(my_email, 'interval', minutes=1)
+#     # scheduler.add_job(my_payment, 'interval', minutes=1)
+#     scheduler.start()
